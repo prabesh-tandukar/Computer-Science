ecb2ab0fae672d9f98802961f6fb7dad7f3210a1
Prabesh <tandukarprabesh@gmail.com>
2025-04-25 13:25:40 +1200

    finger exe 1

---
+++ Intro CS/Lecture 1/fingerExercise1.py
@@ -0,0 +1,9 @@
+# Finger Exercise Lecture 1
+# Assume 3 variables are already defined for you: a, b, and c. Create a variable called total that adds a and b then multiplies the result by c. Include a last line in your code to print the value: print(total)
+
+a = 2
+b = 3
+c = 4
+
+total = ( a + b) * c
+print(total)
