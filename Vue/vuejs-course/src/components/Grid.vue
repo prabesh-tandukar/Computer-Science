804d5e279d10f01a80bab1ffb830f3678d3acdd7
Prabesh <tandukarprabesh@gmail.com>
2025-05-15 14:44:33 +1200

    vue

---
+++ Vue/vuejs-course/src/components/Grid.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { workoutProgram } from "../utils";
+const workoutTypes = ["Push", "Pull", "Legs"];
+</script>
+<template>
+  <section id="grid">
+    <button
+      :key="workoutId"
+      v-for="(workout, workoutIdx) in Object.keys(workoutProgram)"
+      class="card-button plan-card"
+    >
+      <div>
+        <p>Day 1</p>
+        ICON
+      </div>
+      <h3>TYPES</h3>
+    </button>
+  </section>
+</template>
+<style></style>
--- Vue/vuejs-course/src/components/pages/Dashboard.vue
+++ Vue/vuejs-course/src/components/pages/Dashboard.vue
@@ -1,10 +1,11 @@
 <script setup>
+import Grid from "../Grid.vue";
+
 import { gymHealthFacts } from "../../utils";
 
 //generate a randome int between 0 and array length -1
 const randomNumber = Math.floor(Math.random() * gymHealthFacts.length);
 const todaysFact = gymHealthFacts[randomNumber];
-console.log(todaysFact);
 </script>
 
 <template>
@@ -14,8 +15,35 @@ console.log(todaysFact);
       <div>
         <p class="tip"><strong>Daily Tip</strong><br />{{ todaysFact }}</p>
       </div>
+      <button>Start Workout &rarr;</button>
     </div>
+    <Grid />
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.tip-container,
+.tip-container div,
+#dashboard {
+  display: flex;
+}
+
+.tip-container,
+#dashboard {
+  flex-direction: column;
+}
+
+#dashboard {
+  gap: 2rem;
+}
+
+.tip-container {
+  gap: 0.5rem;
+}
+
+@media (min-width: 640px) {
+  .tip-container {
+    gap: 1rem;
+  }
+}
+</style>
